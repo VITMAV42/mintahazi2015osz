57e800bf802b5817236e1b3d7a3ba9c6945b959c
Zsombor Paroczi <zsombor.paroczi@gmail.com>
2015-11-28 15:09:52 +0100

    Delete task

--- middleware/task/deleteTask.js
+++ middleware/task/deleteTask.js
@@ -4,22 +4,18 @@
 module.exports = function (objectrepository) {
   return function (req, res, next) {
 
-    /**
-     * Something like:
-     *  if (typeof res.tpl.task === 'undefined') {
-     *      return next();
-     *  }
-     *
-     *  req.tpl.remove(function(err){
-     *    if (err){
-     *      return next(err);
-     *    }
-     *
-     *    return next();
-     *  });
-     */
+    if (typeof res.tpl.task === 'undefined') {
+      return next();
+    }
 
-    return next();
+    res.tpl.task.remove(function (err) {
+      if (err) {
+        return next(err);
+      }
+
+      //redirect to all tasks
+      res.redirect('/tasks/');
+    });
   };
 
 };
